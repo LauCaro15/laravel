496151c908ee0527ccc43a09b3d3387c1c53dde1
LauCaro15 <lccandamil@gmail.com>
2021-11-22 14:10:17 -0500

    nov22

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,16 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Formulario de regisntro') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre(s)') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
@@ -24,11 +24,39 @@
                                 @enderror
                             </div>
                         </div>
+                        
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Apellidos') }}</label>
+
+                            <div class="col-md-4">
+                                <input id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" 
+                                name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname" 
+                                autofocus placeholder="Apellido Paterno">
+
+                                @error('lastname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-4">
+                                <input id="mothers_lastname" type="text" class="form-control @error('mothers_lastname') is-invalid @enderror" 
+                                name="mothers_lastname" value="{{ old('mothers_lastname') }}" required autocomplete="mothers_lastname" 
+                                autofocus placeholder="Apellido Materno">
+
+                                @error('mothers_lastname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
@@ -40,9 +68,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
@@ -54,9 +82,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Contraseña') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
@@ -64,7 +92,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('Registrarme') }}
                                 </button>
                             </div>
                         </div>
--- resources/views/dashboard/post/form.blade.php
+++ resources/views/dashboard/post/form.blade.php
@@ -1,34 +1,63 @@
-@csrf    
-<div class="form-group mb-3">        
-    <input type="text" class="form-control" name ="name" id ="name"
-    placeholder="Nombre publicación" value= "{{old('name',$post->name)}}" >
+@csrf
+@include('dashboard.partials.validation-error')
+<div class="jumbotron">
+    <div class="form-group row">
+        <label for="name" class="col-md-2 col-form-label text-md-right">{{ __('Publicación') }}</label>
+        <div class="col-md-10">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name"
+                placeholder="Nombre publicación" value="{{ old('name', $post->name) }}">
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+    </div>
+
+    {{-- Lista desplegable dinámica, dependemos de un crud de otro modulo --}}
+    <div class="form-group row">
+        <label for="category_id" class="col-md-2 col-form-label text-md-right">{{ __('Categoría') }}</label>
+        <div class="col-md-4">
+
+            <select class="form-control" name="category_id" id="category_id">
+                @foreach ($categories as $name => $id)
+                    <option value="{{ $id }}"
+                        {{ $post->category_id == $id ? 'selected = "selected"' : '' }}>
+                        {{ $name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <label for="state_post"
+            class="col-md-2 col-form-label text-md-right">{{ __('Estado') }}</label>
+        <div class="col-md-4">
+
+            <select class="form-control" name="state_post" id="state_post">
+                @include('dashboard.components.option-select',['val' => $post->state_post])
+            </select>
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="description" class="col-md-2 col-form-label text-md-right">{{ __('Contenido') }}</label>
+        <div class="col-md-10">
+
+            <textarea id="description" cols="30" name="description" rows="10"
+                class="form-control @error('description') is-invalid @enderror"
+                placeholder="Descripción de la publicación">
+        {{ old('description', $post->description) }}
+    </textarea>
+            @error('description')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+    </div>
+
+    <div class="row-s12 center">
+        <button type="submit" class="btn btn-outline-primary btn-sm">Confirmar</button>
+    </div>
+
 </div>
 
-<div class="form-group mb-3">  
-    <label for="category_id">Categoria</label> 
-    <select class="form-control" name="category_id" id="category_id">
-        @foreach ($categories as $name => $id)
-            <option value="{{ $id }}"{{ $post->category_id == $id ? 'selected = "selected"' : ''}}>
-            {{ $name }}</option>
-        @endforeach
-    </select>     
-</div>
-
-<div class="form-group mb-3">  
-    <label for="state_post">Estado del articulo</label> 
-    <select class="form-control" name="state_post" id="state_post">
-        @include('dashboard.components.option-select', ['val' => $post->state_post])
-    </select>     
-</div>
-
-<div class="form-group mb-3">
-    <label class="form label" for="description">Contenido</label>
-    <textarea name="description" id="description" cols="30" rows="10" class="form-control" placeholder="nombre publicación" >
-    {{old('description',$post->description)}}
-    </textarea>   
-</div>
-    
-<div class="row-s12 center mb-3">
-    <button type="submit" class="btn btn-outline-primary btn-sm">Confirmar</button>
-</div>
 
